a9ca13eb205a18b5959329b4fd88c42923887a08
shima-kou <wakeup1991kou@gmail.com>
2021-07-08 01:54:58 +0900

    VUE課題2-1の内容について完了いたしました。

---
+++ src/views/step3.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="form-container">
+    <div class="form-box">
+      <div class="title-wrap">
+        <span class="step">STEP3</span>
+        <h1 class="title">お客様の情報を入力してください。</h1>
+      </div>
+      <div class="input-wrapper">
+        <div class="box">
+          <h2 class="inquiry-title"><span class="line">ご相談内容</span></h2>
+          <textarea id="messageArea" name="message" cols="30" rows="10"></textarea>
+        </div>
+      </div>
+    </div>
+    <div class="link-wrap">
+      <linkButton to="/step2/" message="前へ戻る" addClass="back-btn"></linkButton>
+      <linkButton to="/step3/" message="次へ進む"></linkButton>
+    </div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import linkButton from '@/components/linkButton.vue';
+
+export default {
+  name: 'Form',
+  components: {
+    linkButton,
+  },
+};
+</script>
